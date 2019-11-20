6413d4a3ec73dda12b4677fddc9f436f109d73b1
Manuel Rios <manu3554@gmail.com>
2019-11-20 08:39:38 -0500

    sdsdfdf

--- ejercicio4.py
+++ ejercicio4.py
@@ -4,13 +4,53 @@ while True:
     if 1<n<6:
         break 
 a =[]
+x = []
+c = []
+d =[]
+e = []
 can_pares=0
 for i in range(n):
-    b = random.randint(1,101)
-    a.append(b)
-    print(b)
-    if b % 2 == 0:
-        can_pares = can_pares+1
-print("el arreglo es",a)
+    if n==2:
+
+        b = random.randint(1,101)
+        a.append(b)
+        x.append(b)
+
+        
+        if b % 2 == 0:
+            can_pares = can_pares+1
+    if n == 3:
+        b = random.randint(1,101)
+        a.append(b)
+        x.append(b)
+        c.append(b)
+        if b % 2 == 0:
+            can_pares = can_pares+1
+    if n == 4:
+        b = random.randint(1,101)
+        a.append(b)
+        x.append(b)
+        c.append(b)
+        d.append(b)
+        if b % 2 == 0:
+            can_pares = can_pares+1
+    
+    if n == 5:
+        b = random.randint(1,101)
+        a.append(b)
+        x.append(b)
+        c.append(b)
+        d.append(b)
+        e.append(b)
+        if b % 2 == 0:
+            can_pares = can_pares+1
+    
+
+print(a)
+print(x)
+print(c)
+print(d)
+print(e)
+
 print("la cantidad de pares es:",can_pares)
 
